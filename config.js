83975695f497184b94919f87766ec45e81e62663
talentdev219 <geektalent3@gmail.com>
2019-05-21 19:34:09 +0700

    Change initial_balance to 100k

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const DIFFICULTY = 2
 
 const MINE_RATE  = 3000  // dynamic difficulty in milisecond
-const INITIAL_BALANCE = 500
-const MINING_REWARD = 50
+const INITIAL_BALANCE = 100000
+const MINING_REWARD = 25000
 
 module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD }
