7340d24f169bc9815c1720219c9703ce5bdc348c
talentdev219 <geektalent3@gmail.com>
2019-09-10 14:42:10 +0700

    add mongoose for dependency

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 /**
- *  difficulty for mining
+ *  difficulty for miningg
  * use for test & app itself
  * it uses dynamic mining difficulty that change on certain time */
 
-const DIFFICULTY = 6
+const DIFFICULTY = 3
 
-const MINE_RATE  = 5000  // dynamic difficulty in milisecond
+const MINE_RATE  = 2000  // dynamic difficulty in milisecond
 const INITIAL_BALANCE = 100000
 
 module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE }
--- dev-test.js
+++ dev-test.js
@@ -1,18 +1,6 @@
 // file untuk nguji aplikasi secara keseluruhan
 // dilakukan setelah unit test 
 
-/* test the block and chain
-const Blockchain = require('./blockchain')
-
-const bc = new Blockchain()
-
-for(let i=0; i<10; i++){
-  console.log(bc.addBlock(`foo ${i}`).toString())
-}
-
-console.log("test pass")
-*/
-
 // test the wallet 
 const Wallet = require('./wallet ')
 const wallet = new Wallet()
